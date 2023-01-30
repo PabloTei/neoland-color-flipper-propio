971b2a70b99998f2082af9390a5f178997773430
PabloTei <pteijeirolopez@gmail.com>
2023-01-30 16:51:09 +0100

    iteration1

---
+++ main.js
@@ -0,0 +1,30 @@
+import './style.css'
+
+const colors = {
+  "#476A6F": "Deep Space Sparkle",
+  "#519E8A": "Polished Pine",
+  "#7EB09B": "Green Sheen",
+  "#C5C9A4": "Sage",
+  "#ECBEB4": "Baby Pink"
+}
+
+const selectColor = document.querySelector("select");
+const colorName = document.querySelector("#color-name")
+
+Object.keys(colors).forEach((color) => {
+  const option = document.createElement("option");
+  option.value = color;
+  option.innerText = colors[color];
+  selectColor.appendChild(option);
+})
+
+selectColor.addEventListener("change", (event) => {
+  const newColor = event.target.value;
+  document.body.style.backgroundColor = newColor;
+  colorName.innerText = `${colors[newColor]} | ${newColor}`;
+})
+
+
+
+
+
